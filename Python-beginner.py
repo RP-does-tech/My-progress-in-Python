eb9b007bc6184d2e99fb2535b053e91f9f99391b
RP-does-tech <katsukisenpai12@gmail.com>
2026-09-23 14:20:17 +0800

    Add files via upload

---
+++ Python-beginner.py
@@ -0,0 +1,51 @@
+#What I learned on Python basic during my study 
+
+#Print command in Python
+print("Hello World!")
+
+#Variable in Python
+#There are a lot of variables in Python such as integer, string, float, boolean etc
+
+#Integer in Python
+number = 1
+
+#String in Python
+name = "RP"
+
+#Float in Python where it matter in decimals
+float = 3.142
+
+#Boolean in Python
+#where it involves either True or False
+status = True 
+
+#List in Python 
+#Listing items or number in a squared bracket with commas.
+scores = [2,4,6,8]
+
+#Simple Mathematics symbol in Python
+#Addition, Subraction, Multiplication and Division
+
+#Addition in Python using the + symbol
+num = 1 + 2
+print(num)
+
+#Substraction in Python using the - symbol
+num = 1 - 2
+print(num)
+
+#Multiplication in Python using the * symbool
+num = 1 * 2
+print(num)
+
+#Division in Python using the / symbol
+num = 1/2
+print(num)
+
+#To add spacing in between use the \m command
+name = "RP"
+age = 22
+status = True
+print("\n=============")
+print(name)
+print(age)
